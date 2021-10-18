2ff2b0063924f9fd90fdc19bfd91e7744e3862f0
Dan Lambright <dlambrig@gmail.com>
2021-10-17 22:18:01 -0400

    more changes for week8

--- Chapter09/sample3/src/main/java/com/leszko/calculator/CalculatorController.java
+++ Chapter09/sample3/src/main/java/com/leszko/calculator/CalculatorController.java
@@ -27,7 +27,7 @@ class CalculatorController {
         @RequestMapping("/div")
         String div(@RequestParam("a") Integer a,
                         @RequestParam("b") Integer b) {
-                String result = b!=0 ? String.valueOf(calculator.sum(a, b)) : "Division by 0";
+                String result = b!=0 ? String.valueOf(calculator.div(a, b)) : "Division by 0";
                 calculationRepository.save(new Calculation(a.toString(), b.toString(), result, Timestamp.from(Instant.now())));
                 return result;
         }
